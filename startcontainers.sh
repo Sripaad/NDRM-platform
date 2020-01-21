cf1cd5a1992e9067e3ca91b14f9869f04c720af6
Sripaad <sripaad751@gmail.com>
2020-01-22 00:58:10 +0530

    added services

--- startcontainers.sh
+++ startcontainers.sh
@@ -1,5 +1,6 @@
 # just the first time
+# cd scrapper
 # docker build -t scrapper-image:latest .
 # docker run --name scrapperapp -v$PWD/app:/app -p 5000:5000 scrapper-image:latest
 
-docker-compose up -d
+docker-compose up 
